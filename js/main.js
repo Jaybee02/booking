b7f3c9e8235d3554cb34b854aa19d8397781bdcb
Jaybee02 <ogundipeajibike002@gmail.com>
2022-07-05 18:20:46 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function passwordToggle(){
+    var input = document.getElementById("password1");
+    var eyeOpen = document.getElementById("eye-open1");
+    var eyeClosed = document.getElementById("eye-closed1");
+
+    if (input.type ==="password"){
+      input.type = "text";
+      eyeClosed.style.display = "none";
+      eyeOpen.style.display = "block";
+    }
+    else{
+        input.type = "password";
+        eyeOpen.style.display = "none";
+        eyeClosed.style.display = "block";  
+    }
+}
